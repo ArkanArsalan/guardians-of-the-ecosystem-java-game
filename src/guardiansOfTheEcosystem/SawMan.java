4365e60e97ab84ef09271a588988d09d0eab0fd9
Arkan Arsalan <arkan.arsalan.amanullah@gmail.com>
2023-12-17 23:57:33 +0700

    Change structure of the class

--- src/guardiansOfTheEcosystem/SawMan.java
+++ src/guardiansOfTheEcosystem/SawMan.java
@@ -2,13 +2,11 @@ package guardiansOfTheEcosystem;
 
 public class SawMan extends Enemy {
 	
-	private static final int ATTACK_RANGE = 50;
+	private static final int HEALTH = 1000;
+	private static final int MOVE_SPEED = 1;
 	
 	public SawMan(World gp, int lane) {
-		super(gp, 1000, 1, lane);
+		super(gp, HEALTH, MOVE_SPEED, lane);
 	}
-	
-	
-	
 	
 }
