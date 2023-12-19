21e701cc775ef8c990a13e988049d3bf27d8a6d6
Birhasani <birhasani8ra@gmail.com>
2023-12-19 14:24:57 +0700

    finishing

--- src/Panels/GameOverPanel.java
+++ src/Panels/GameOverPanel.java
@@ -1,53 +1,79 @@
 package Panels;
 
 import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.Font;
+import java.awt.Graphics;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.awt.Image;
 import java.awt.event.ActionEvent;
+import java.io.IOException;
+import java.net.URL;
 
+import javax.imageio.ImageIO;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class GameOverPanel extends JPanel {
 	
+	private Image backgroundImage;
+	 
     public GameOverPanel() {
-        // Set the background color to red
-        setBackground(Color.RED);
-
-        // Use GridBagLayout for flexibility in component placement
+    	// Load the background image
+        try {
+            URL resource = getClass().getResource("images/gameover.jpg");
+            backgroundImage = ImageIO.read(resource);
+        } catch (IOException e) {
+            e.printStackTrace(); // Handle the exception appropriately
+        }
+        
+     // Set the layout and background color
         setLayout(new GridBagLayout());
-        GridBagConstraints gbc = new GridBagConstraints();
-        gbc.gridx = 0;
-        gbc.gridy = 0;
-        gbc.insets = new java.awt.Insets(10, 10, 10, 10); // Add some padding
+        setBackground(new Color(230, 230, 255)); // You can adjust the RGB values
 
-        // Create a game over label
-        JLabel gameOverLabel = new JLabel("Game Over");
-        gameOverLabel.setFont(new Font("Arial", Font.BOLD, 36));
-        gameOverLabel.setForeground(Color.WHITE); // Set text color to white
-
-        // Create a label for the message
-        JLabel messageLabel = new JLabel("The Enemy has Destroyed the Ecosystem.");
-        messageLabel.setFont(new Font("Arial", Font.PLAIN, 18));
-        messageLabel.setForeground(Color.WHITE); // Set text color to white
+        // Initialize components and add them to the panel
+        initComponents();
+    }
+    
+    private void initComponents() {
+    	
+    	setLayout(null); // Set layout to null for absolute positioning
+        // Use GridBagLayout for flexibility in component placement
 
         // Create a restart button and bind an action listener to switch panels
-        JButton restartButton = new JButton("Back to Main Menu");
+        JButton restartButton = new JButton("");
         restartButton.addActionListener(this::restartGame);
 
+        
+     // Set the button to be transparent
+        restartButton.setOpaque(false);
+        restartButton.setContentAreaFilled(false);
+        restartButton.setBorderPainted(false);
+
+        // Set the X and Y positions for the button
+        restartButton.setBounds(420, 520, 100, 50); // Adjust X, Y, width, and height as needed
+        restartButton.setPreferredSize(new Dimension(200, 100));
+
         // Add components to the panel with centered layout
-        add(gameOverLabel, gbc);
-        gbc.gridy++;
-        add(messageLabel, gbc);
-        gbc.gridy++;
-        add(restartButton, gbc);
+
+        add(restartButton);
     }
     
     private void restartGame(ActionEvent e) {
         GamePanel parent = (GamePanel) getParent();
         parent.showPanel("MainMenu");
+    }
+    
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+
+        // Draw the background image
+        if (backgroundImage != null) {
+            g.drawImage(backgroundImage, 0, 0, getWidth(), getHeight(), this);
+        }
     }
     
 }
